4dc4cfd2c3750fd7ec6dbdc1d28354f112e4125e
neo <st2udio@gmail.com>
2022-10-13 09:43:05 +0800

    fix leave room error

--- server/internal/logic/gtcserver/join_room_logic.go
+++ server/internal/logic/gtcserver/join_room_logic.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/metagogs/gogs/group"
 	"github.com/metagogs/gogs/gslog"
 	"github.com/metagogs/gogs/session"
 	"github.com/szpnygo/gtc/server/internal/message"
@@ -35,6 +36,15 @@ func (l *JoinRoomLogic) Handler(in *model.JoinRoom) {
 	uid := fmt.Sprintf("%d_%s", l.session.ID(), in.Name)
 	l.session.SetUID(uid)
 
+	// when the session is closed, remove the user from the room
+	l.session.SetOnCloseCallback(func(id int64) {
+		for _, name := range l.svcCtx.GroupList {
+			if g, ok := l.svcCtx.GS.GetGroup(name); ok {
+				l.removeUserFromGroup(g, in.Name)
+			}
+		}
+	})
+
 	group, exist := l.svcCtx.GS.GetGroup(in.RoomId)
 	if !exist {
 		// the room does not exist
@@ -42,34 +52,37 @@ func (l *JoinRoomLogic) Handler(in *model.JoinRoom) {
 	}
 
 	// join room
-	if err := group.AddUser(l.ctx, l.session.UID()); err == nil {
-		users := group.GetUsers(l.ctx)
-		usersFilted := filterUid(users)
-
-		_ = message.SendJoinRoomSuccess(l.session, &model.JoinRoomSuccess{
-			RoomId: group.GetGroupName(l.ctx),
-			UserId: l.session.ID(),
-			Users:  usersFilted,
-		})
-
-		session.BroadcastMessage(users, &model.JoinRoomNotify{
-			RoomId: group.GetGroupName(l.ctx),
-			Name:   in.Name,
-			UserId: l.session.ID(),
-			Users:  usersFilted,
-		}, nil, l.session.UID())
-
-		// when the session is closed, remove the user from the room
-		l.session.OnClose(func(id int64) {
-			_ = group.RemoveUser(l.ctx, l.session.UID())
-			session.BroadcastMessage(users, &model.LeaveRoomNotify{
-				RoomId: group.GetGroupName(l.ctx),
-				Name:   in.Name,
-				Users:  filterUid(group.GetUsers(l.ctx)),
-			}, nil)
-		})
+	if err := group.AddUser(l.ctx, l.session.UID()); err != nil {
+		return
 	}
 
+	users := group.GetUsers(l.ctx)
+	usersFilted := filterUid(users)
+
+	_ = message.SendJoinRoomSuccess(l.session, &model.JoinRoomSuccess{
+		RoomId: group.GetGroupName(l.ctx),
+		UserId: l.session.ID(),
+		Users:  usersFilted,
+	})
+
+	session.BroadcastMessage(users, &model.JoinRoomNotify{
+		RoomId: group.GetGroupName(l.ctx),
+		Name:   in.Name,
+		UserId: l.session.ID(),
+		Users:  usersFilted,
+	}, nil, l.session.UID())
+}
+
+func (l *JoinRoomLogic) removeUserFromGroup(g group.Group, name string) {
+	if err := g.RemoveUser(l.ctx, l.session.UID()); err == nil {
+		// broadcast the user left message if user is in the room
+		list := g.GetUsers(l.ctx)
+		session.BroadcastMessage(list, &model.LeaveRoomNotify{
+			RoomId: g.GetGroupName(l.ctx),
+			Name:   name,
+			Users:  filterUid(list),
+		}, nil)
+	}
 }
 
 func filterUid(uids []string) []*model.User {
